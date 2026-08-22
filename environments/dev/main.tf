2383fbaeed236f5f3bb8618e49cd50ea417bac17
shajugit <v.shajahan1996@gmail.com>
2026-08-22 23:25:21 +0530

    terraform

---
+++ environments/dev/main.tf
@@ -0,0 +1,23 @@
+module "aws-eks-VPC" {
+    source = "../../modules/01-vpc"
+    project = var.project
+    environment = var.environment
+}
+
+module "aws-eks-SG" {
+    source = "../../modules/02-sg"
+    project = var.project
+    environment = var.environment
+}
+
+module "aws-eks-cluster" {
+    source = "../../modules/03-eks"
+    project = var.project
+    environment = var.environment
+}
+
+module "aws-eks-bastion" {
+    source = "../../modules/04-bastion"
+    project = var.project
+    environment = var.environment
+}
