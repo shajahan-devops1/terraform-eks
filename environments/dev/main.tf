87a73127706d8b4b6c0eb308ef67205d3d56408d
shajugit <v.shajahan1996@gmail.com>
2026-08-23 12:18:10 +0530

    terraform

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,26 +5,27 @@ module "aws-eks-VPC" {
 }
 
 module "aws-eks-SG" {
-  source = "../../modules/02-sg"
-
+  source      = "../../modules/02-1-SG"
   project     = var.project
   environment = var.environment
+  vpc_id      = module.aws-eks-VPC.vpc_id
+  sg_name     = "${var.project}-${var.environment}-sg"
 }
 
-module "aws-eks-SG-rules" {
-  source      = "../../modules/02-sg"
-  project     = var.project
-  environment = var.environment
-}
+# module "aws-eks-SG-rules" {
+#   source      = "../../modules/02-sg"
+#   project     = var.project
+#   environment = var.environment
+# }
 
-module "aws-eks-cluster" {
-  source      = "../../modules/03-eks"
-  project     = var.project
-  environment = var.environment
-}
+# module "aws-eks-cluster" {
+#   source      = "../../modules/03-eks"
+#   project     = var.project
+#   environment = var.environment
+# }
 
-module "aws-eks-bastion" {
-  source      = "../../modules/04-bastion"
-  project     = var.project
-  environment = var.environment
-}
+# module "aws-eks-bastion" {
+#   source      = "../../modules/04-bastion"
+#   project     = var.project
+#   environment = var.environment
+# }
