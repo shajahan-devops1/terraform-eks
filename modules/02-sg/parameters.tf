b4f9cee7f34f2b95b56dad42389140d22e3910ea
shajugit <v.shajahan1996@gmail.com>
2026-08-23 10:32:58 +0530

    terraform

--- modules/02-sg/parameters.tf
+++ modules/02-sg/parameters.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "sg_id" {
   count     = length(var.sg_names)
   name      = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id" # /roboshop/dev/backend_alb_sg_id, /roboshop/dev/mongodb_sg_id
   type      = "String"
-  value     = aws_security_group[count.index].id
+  value     = aws_security_group.main[count.index].id
   overwrite = true
 }
