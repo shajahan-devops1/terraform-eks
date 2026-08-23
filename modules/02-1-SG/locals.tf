ce50fd611bdd00de82ba22589b609cfa43a8a4ae
shajugit <v.shajahan1996@gmail.com>
2026-08-23 12:36:19 +0530

    terraform

--- modules/02-1-SG/locals.tf
+++ modules/02-1-SG/locals.tf
@@ -1,10 +1,10 @@
 locals {
-  bastion_sg_id          = aws_security_group.sg["bastion"].id
-  mongodb_sg_id          = aws_security_group.sg["mongodb"].id
-  redis_sg_id            = aws_security_group.sg["redis"].id
-  mysql_sg_id            = aws_security_group.sg["mysql"].id
-  rabbitmq_sg_id         = aws_security_group.sg["rabbitmq"].id
-  public_alb_sg_id       = aws_security_group.sg["public-alb"].id
-  eks_control_plane_sg_id = aws_security_group.sg["eks-control-plane"].id
-  eks_node_sg_id         = aws_security_group.sg["eks-node"].id
+  bastion_sg_id           = aws_security_group.sg["bastion"].id
+  mongodb_sg_id           = aws_security_group.sg["mongodb"].id
+  redis_sg_id             = aws_security_group.sg["redis"].id
+  mysql_sg_id             = aws_security_group.sg["mysql"].id
+  rabbitmq_sg_id          = aws_security_group.sg["rabbitmq"].id
+  public_alb_sg_id        = aws_security_group.sg["public_alb"].id
+  eks_control_plane_sg_id = aws_security_group.sg["eks_control_plane"].id
+  eks_node_sg_id          = aws_security_group.sg["eks_node"].id
 }
