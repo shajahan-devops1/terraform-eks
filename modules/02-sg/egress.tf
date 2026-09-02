46556b7aa812e7e0bf21b6c02be45e53a9e53cdf
shajugit <v.shajahan1996@gmail.com>
2026-09-02 09:51:43 +0530

    terraform

--- modules/02-sg/egress.tf
+++ modules/02-sg/egress.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc_security_group_egress_rule" "eks_node" {
-  security_group_id = aws_security_group.sg["eks_node"].id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "eks_node" {
+#   security_group_id = aws_security_group.sg["eks_node"].id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
